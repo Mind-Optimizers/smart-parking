f5d9a87510712c43eb54ef379b59ba153ce759f1
Swaraj <swarajsingpatil@gmail.com>
2020-06-22 19:00:18 +0530

    add maps

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,18 +1,24 @@
 import React from 'react'
 import { View, StyleSheet, StatusBar } from 'react-native'
-import { Text, Appbar } from 'react-native-paper';
-import { primary } from '../constants';
+import { Text, Appbar } from 'react-native-paper'
+import { primary } from '../constants'
+import MapView from 'react-native-maps'
+import { TextInput } from 'react-native'
 
 const Home = () => {
 
-    StatusBar.setBackgroundColor(primary)
+    StatusBar.setTranslucent(true)
+    StatusBar.setBarStyle("dark-content")
 
     return (
-        <View>
-            <Appbar>
-                <Text style={styles.heading}>Home</Text>
-            </Appbar>
+        <View style={styles.container}>
+            <View style={{marginHorizontal: 20}}>
+                <TextInput style={styles.searchBar} onChangeText={text => onChangeText(text)} placeholder="Search Parking Zone" />
+            </View>
+            <MapView style={{flex: 1}}/>
+            
         </View>
+        
     )
 }
 
@@ -20,6 +26,21 @@ const styles = StyleSheet.create({
     heading: {
         marginLeft: 10,
         fontSize: 24,
+    },
+    container: {
+        flex: 1,
+        
+    },
+    searchBar:{
+        position: "absolute",
+        height: 40,
+        borderRadius: 200,
+        backgroundColor: "#FFF",
+        top: 70,
+        width: "100%",
+        elevation: 4,
+        padding: 10,
+    
     }    
 })
 
--- src/screens/SignIn.js
+++ src/screens/SignIn.js
@@ -2,7 +2,10 @@ import React from 'react'
 import { Surface, Button } from 'react-native-paper'
 import { StyleSheet, Text, View, Image, StatusBar } from 'react-native'
 
-const SignIn = () => {
+const SignIn = (props) => {
+    const goToHome = () => {
+        props.navigation.navigate("Home")        
+    }
     StatusBar.setBackgroundColor('#fff')
     StatusBar.setBarStyle('dark-content')
     return (
@@ -22,10 +25,10 @@ const SignIn = () => {
             <View style={{
                 marginHorizontal: 20,
             }}> 
-                <Button style={styles.button} mode="contained" color="#ea4335" icon="google">
+                <Button style={styles.button} mode="contained" color="#ea4335" icon="google" onPress={goToHome}>
                     Sign In With Google
                 </Button>
-                <Button style={styles.button} mode="contained" color="#3b5998" icon="facebook">
+                <Button style={styles.button} mode="contained" color="#3b5998" icon="facebook" onPress={goToHome}>
                     Sign In With Facebook
                 </Button>
             </View>
@@ -42,7 +45,9 @@ const styles = StyleSheet.create({
     },
     image: {
         width: '100%',
-        height: 200,
+        height: 250,
+         
+        
     },
     button: {
         marginVertical: 10
