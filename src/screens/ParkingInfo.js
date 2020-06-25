5eeeeb8b7167c65e8a53d141d57b8984de7c204b
YashShettigar <47048488+YashShettigar@users.noreply.github.com>
2020-06-25 19:24:17 +0530

    completed parking info

--- src/screens/ParkingInfo.js
+++ src/screens/ParkingInfo.js
@@ -1,29 +1,35 @@
 import React from 'react'
-import { View, Image, Text, StatusBar } from 'react-native'
+import { View, Image, StatusBar, Text } from 'react-native'
 
 //styles
 import { ParkingInfoStyles as styles } from '../styles/ParkingInfoStyles'
-import { IconButton } from 'react-native-paper'
+import { IconButton, FAB } from 'react-native-paper'
+import { primary } from '../constants'
+
 
 // fetch location data
 // import { getParkingSpace } from '../backend/FetchLocations'
 
 import Home from '../screens/Home'
 
-export default ParkingInfo = () => {
+
+export default ParkingInfo = ({ navigation }) => {
+
     StatusBar.setBackgroundColor('transparent')
     StatusBar.setBarStyle("light-content")
+
     return (
         <View style={ styles.container }>
-            <IconButton
-                color="#fff"
-                icon="chevron-left"
-                style={ styles.backBtn }
-                size={40}
-            />
             <Image 
                 source={{uri: 'https://images.unsplash.com/photo-1524214786335-66456317bde6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'}}
                 style={ styles.thumbnails }
+            />
+            <IconButton
+                icon="chevron-left"
+                style={ styles.backBtn }
+                color="#fff"
+                size={45}
+                onPress={() => navigation.goBack()}
             />
             <View style={ styles.locationDetails }>
                 <Text style={ styles.locationName }>
@@ -33,26 +39,24 @@ export default ParkingInfo = () => {
                     gysgkd fkbaj bmbm bfjsd mfhmdgfh dmhfhm sgjhfghmd ghfg shj gjf gshdgfjsgdjf ghjsgjf gjhsg dfg u4t w yru3 ty7r y83y783y8u93
                 </Text>
                 <View style={styles.timeContainer}>
-                    <View style={styles.durationText}>
-                        <Text style={ styles.timeText }>Opening Time: </Text>  
-                        <Text style={ styles.timeText }>23:00:56</Text>
-                    </View>
-                    <View style={styles.durationText}>
-                        <Text style={ styles.timeText }>Closing Time: </Text>  
-                        <Text style={ styles.timeText }>12:00:34</Text>
-                    </View>
+                    <Text 
+                        style={styles.durationText}
+                    >Opening Time: </Text>  
+                    <Text style={ { marginBottom: 10 }, styles.timeText }>23:00:56</Text>
+                    <Text 
+                        style={styles.durationText}
+                    >Closing Time: </Text>  
+                    <Text style={ styles.timeText }>12:00:34</Text>
                 </View>
             </View>
-            {/* <IconButton 
-                color="#786372"
+            <FAB
+                backgroundColor={primary}
                 icon="ticket-confirmation"
                 style={styles.bookBtn}
-                size={20}
-            >
-                <Text style={styles.bookBtnText}>
-                    Book Now
-                </Text>
-            </IconButton> */}
+                size={20} 
+                label="Book Now"
+                onPress={() => console.log("jsdgj")}
+            />
         </View>
     )
 }
--- src/screens/index.js
+++ src/screens/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, {useState, useEffect} from 'react'
 import { View, Text, Image } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
@@ -64,6 +65,18 @@ const CustomContent = props => {
 
 const FinDrawer = connect(({user}) => ({user}))(CustomContent)
 
+const ParkingNavigation = props => {
+    return (
+        <Stack.Navigator
+            screenOptions={{
+                headerShown: false
+            }}
+        >
+            <Stack.Screen name="Book" component={ParkingInfo} />
+        </Stack.Navigator>
+    )
+}
+
 const HomeNavigation = props => {
     return (
         <Drawer.Navigator
@@ -74,8 +87,8 @@ const HomeNavigation = props => {
             name="Home" 
             component={Home}/>
             <Drawer.Screen 
-            name="Book" 
-            component={ParkingInfo}/>
+            name="ParkingNavigation" 
+            component={ParkingNavigation}/>
         </Drawer.Navigator>
     )
 }
@@ -128,3 +141,142 @@ const AppNavigator = props => {
 const mapStateToProps = ({user}) => ({user})
 
 export default connect(mapStateToProps, {loginUser})(AppNavigator)
+=======
+import React, {useState, useEffect} from 'react'
+import { View, Text, Image } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator,  DrawerContentScrollView, DrawerItem } from '@react-navigation/drawer';
+import { Drawer as PaperDrawer } from 'react-native-paper';
+import { connect } from 'react-redux';
+import auth from '@react-native-firebase/auth'
+import Splash from './Splash';
+import Home from './Home';
+import Profile from './Profile';
+import SignIn from './SignIn';
+import { loginUser } from '../state/actions';
+import { useRoute } from '@react-navigation/native';
+
+import ParkingInfo from '../screens/ParkingInfo'
+
+const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+
+const CustomContent = props => {
+    const route = useRoute();
+    const {user, navigation} = props;
+    return (
+        <DrawerContentScrollView {...props}>
+            <View style={{
+                paddingVertical: 10,
+                paddingHorizontal: 20,
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'space-between'
+            }}>
+                <Image 
+                    source={{
+                        uri: user.dp
+                    }}
+                    style={{
+                        width: 50,
+                        height: 50,
+                        borderRadius: 25
+                    }}
+                />
+                <Text>{user.name}</Text>
+            </View>
+            <PaperDrawer.Item 
+            icon="home"
+            label="Home"
+            active={route.name === 'Home'}
+            />
+            <PaperDrawer.Item 
+            icon="face-profile"
+            label="Profile"
+            onPress={() => navigation.navigate("Profile")}
+            active={route.name === 'Profile'}
+            />
+            <PaperDrawer.Item 
+            icon="face-profile"
+            label="Book"
+            active={route.name === 'Book'}
+            onPress={() => navigation.navigate('Book')}
+            />
+        </DrawerContentScrollView>
+    )
+}
+
+const FinDrawer = connect(({user}) => ({user}))(CustomContent)
+
+const HomeNavigation = props => {
+    return (
+        <Drawer.Navigator
+            drawerContent={_props => <FinDrawer {..._props}/>}
+        >
+
+            <Drawer.Screen 
+            name="Home" 
+            component={Home}/>
+            <Drawer.Screen 
+            name="Book" 
+            component={ParkingInfo}/>
+      
+            <Drawer.Screen 
+            name="Profile" 
+            component={Profile}/>
+
+        </Drawer.Navigator>
+    )
+}
+
+const AppNavigator = props => {
+
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        const unsubscribe = auth().onAuthStateChanged(user => {
+            if (user) {
+                props.loginUser({
+                    name: user.displayName,
+                    id: user.uid,
+                    email: user.email,
+                    dp: user.photoURL
+                })
+                console.log(props.user)
+            }
+            if (loading) setLoading(false)
+        })
+
+        return unsubscribe
+    }, [])
+
+    if (loading) {
+        return <Splash />
+    }
+    const {user} = props;
+    return (
+        <NavigationContainer>
+            <Stack.Navigator
+                screenOptions={{
+                    headerShown: false,
+                }}
+            >
+                {user.id ? <Stack.Screen 
+                    name="Home"
+                    component={HomeNavigation}
+                    /> : 
+                    <Stack.Screen 
+                name="SignIn" 
+                component={SignIn} />
+                }
+            </Stack.Navigator>
+        </NavigationContainer>
+    ) 
+}
+
+const mapStateToProps = ({user}) => ({user})
+
+export default connect(mapStateToProps, {loginUser})(AppNavigator)
+>>>>>>> 0ab3cf3a17892b01e2bc2039ae2ec2a7a5a9f68e
